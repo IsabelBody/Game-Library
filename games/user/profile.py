96bfc217316575b1e50e0bcac5eb6b6ef45cc0f1
Isabel Body <isabelbody@gmail.com>
2023-09-09 15:48:16 +1200

    initialised the profile page with its panels and decorated with css

--- games/user/profile.py
+++ games/user/profile.py
@@ -1,4 +1,11 @@
-from flask import Blueprint, request, url_for
-
+from flask import Blueprint, request, render_template
 
 profile_blueprint = Blueprint('profile_bp', __name__)
+
+@profile_blueprint.route('/profile', methods=['GET'])
+def profile():
+    return render_template(
+        'profile.html',
+        title="Profile",
+        heading='My Profile',
+        )
