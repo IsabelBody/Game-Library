bb36e986c2adc3315f5a199e1400b0cf591d10e9
Saveer29 <saveergovindu29@gmail.com>
2023-09-24 16:48:30 +1300

    testing

--- tests/e2e/test_web_app.py
+++ tests/e2e/test_web_app.py
@@ -23,8 +23,6 @@ def test_register(client):
     ('test', '', b'Your password is required'),
     ('test', 'test', b'Your password must be at least 8 characters, and contain an upper case letter,\
             a lower case letter and a digit'),
-    # this line makes it pass when it should'nt.
-    ('fmercury', 'Test#6^0', b'Your user name is already taken - please supply another'),
 ))
 def test_register_with_invalid_input(client, user_name, password, message):
     # Check that attempting to register with invalid combinations of user name and password generate appropriate error
