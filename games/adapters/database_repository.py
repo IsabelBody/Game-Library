e23a12c320868b3482df303acea720cfedb83f9c
seifFarah <sfar671@aucklanduni.ac.nz>
2023-09-28 21:29:17 +1300

    all database repo methods

--- games/adapters/database_repository.py
+++ games/adapters/database_repository.py
@@ -9,12 +9,10 @@ from games.adapters.repository import AbstractRepository
 from games.domainmodel.model import User, Game, Genre, Review, Publisher, Wishlist
 
 
-
 class SessionContextManager:
     def __init__(self, session_factory):
         self.__session_factory = session_factory
         self.__session = scoped_session(self.__session_factory)
-
 
     def __enter__(self):
         return self
@@ -61,7 +59,7 @@ class SqlAlchemyRepository(AbstractRepository):
     def get_user(self, user_name: str) -> User:
         user = None
         try:
-            user = self._session_cm.session.query(User).filter(User._User__username == user_name).one()
+            user = self._session_cm.session.query(User).filter(User.username == user_name).one()
         except NoResultFound:
             pass
 
@@ -95,7 +93,8 @@ class SqlAlchemyRepository(AbstractRepository):
         return genres_all
 
     def get_games_for_genre(self, genre_name) -> List[Game]:
-        games_for_genre = self._session_cm.session.query(Game).join(Game.genres).filter(Genre.genre_name == genre_name).all()
+        games_for_genre = self._session_cm.session.query(Game).join(Game.genres).filter(
+            Genre.genre_name == genre_name).all()
 
         return games_for_genre
 
@@ -111,3 +110,38 @@ class SqlAlchemyRepository(AbstractRepository):
         num_unique = self._session_cm.session.query(Genre).all().distinct().count()
         return num_unique
 
+    def add_wishlist(self, user: User):
+        with self._session_cm as scm:
+            wishlist = Wishlist(user)
+            scm.session.add(wishlist)
+            scm.commit()
+
+    def add_wishlist_game(self, user: User, game: Game):
+        with self._session_cm as scm:
+            user_wishlist = scm.session.query(Wishlist).filter(Wishlist.user == user).first()
+            if user_wishlist is None:
+                user_wishlist = Wishlist(user)
+                scm.session.add(user_wishlist)
+            user_wishlist.games.append(game)
+            scm.commit()
+
+    def remove_wishlist_game(self, user: User, game: Game):
+        with self._session_cm as scm:
+            user_wishlist = scm.session.query(Wishlist).filter(Wishlist.user == user).first()
+            if user_wishlist and game in user_wishlist.games:
+                user_wishlist.games.remove(game)
+                scm.commit()
+
+    def get_wishlist_games(self, user: User) -> List[Game]:
+        with self._session_cm as scm:
+            user_wishlist = scm.session.query(Wishlist).filter(Wishlist.user == user).first()
+            if user_wishlist:
+                return user_wishlist.games
+            return []
+
+    def add_review_to_game(self, game_id, review):
+        with self._session_cm as scm:
+            game = scm.session.query(Game).filter(Game.game_id == game_id).first()
+            if game:
+                game.reviews.append(review)
+                scm.commit()
